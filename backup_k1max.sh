1702ec6b62106dc12158eae79b44655ece596f3c
Scott McCrory <scott@mccrory.us>
2026-02-11 22:12:51 -0500

    Mid-feb update

--- backup_k1max.sh
+++ backup_k1max.sh
@@ -1,5 +1,4 @@
-scp -O -r root@192.168.4.183:/usr/data/printer_data .
+scp -r k1max:/usr/data/printer_data .
 rm  -rf printer_data/config/kamp/.git
 rm  -rf printer_data/config/kamp/.github
 rm  -rf printer_data/gcodes
-
